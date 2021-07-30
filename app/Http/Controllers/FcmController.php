196ae0ec9e752e3baf123dc4a5da4c8c763d3bac
sanghun yang <eogksusa@gmail.com>
2021-07-30 06:32:20 +0000

    알림 테스트 0730

--- app/Http/Controllers/FcmController.php
+++ app/Http/Controllers/FcmController.php
@@ -33,12 +33,12 @@ class FcmController extends Controller
     public function sendNoti()
     {
 
-        $tokens = DB::table('tbl_user_token')->selectRaw('token')->get();
-
+        $tokens = DB::table('tbl_user_token')->selectRaw('token')->get();        
         
         $from = "AAAAV-0WwLk:APA91bF_luycW3zLbnAHNi5QESV6YPqRn9FhjNFuHE3O3RSC0jb20_1ddfEHKgCGOCeCNXST8xAcfDSJE7pnZuKpPcK-B9KcVKOdR9t4-G4R_MoUp_Cphgfq2gRKuglnbujgYsdY5L76";
-        foreach($tokens as $key => $val):
-            $token = $val->token;
+        for($i=0;$i<sizeof($tokens);$i++){
+
+            $token = $tokens[$i]->token;
 
             $msg = array
               (
@@ -49,32 +49,32 @@ class FcmController extends Controller
                 'sound' => 'mySound'
               );
 
-        $fields = array
-                (
-                    'to'        => $token,
-                    'notification'  => $msg
-                );
+            $fields = array
+                    (
+                        'to'        => $token,
+                        'notification'  => $msg
+                    );
 
-        $headers = array
-                (
-                    'Authorization: key=' . $from,
-                    'Content-Type: application/json'
-                );
-        
-        //#Send Reponse To FireBase Server         
-        $ch = curl_init();
+            $headers = array
+                    (
+                        'Authorization: key=' . $from,
+                        'Content-Type: application/json'
+                    );
+            
+            //#Send Reponse To FireBase Server         
+            $ch = curl_init();
 
-        curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
-        curl_setopt( $ch,CURLOPT_POST, true );
-        curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
-        curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
-        curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
-        $result = curl_exec($ch );
-        dd($result);
-        curl_close( $ch );
+            curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+            curl_setopt( $ch,CURLOPT_POST, true );
+            curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+            curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+            curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+            curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+            $result = curl_exec($ch );
+            dd($result);
+            curl_close( $ch );
 
-        endforeach;
+        }
 
     }
 }
